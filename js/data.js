60243a50f3bc56f6c5803b4df8426f53d8606db8
sat048 <123526479+sat048@users.noreply.github.com>
2023-11-18 14:12:59 -0500

    Add files via upload

--- js/data.js
+++ js/data.js
@@ -1,29 +1,29 @@
-// function addClass() {
-//     var classInput = document.getElementById('classInput');
-//     var course = classInput.value.trim();
-
-//     if (course !== '') {
-//         var listItem = document.createElement('li');
-//         listItem.textContent = skill;
-
-//         var classList = document.getElementById('classList');
-//         classList.appendChild(listItem);
-
-//         classInput.value = '';
-//     }
-// }
-
-document.querySelector("#addClassBtn").addEventListener("click", function() {
+function addClass() {
     var classInput = document.getElementById('classInput');
     var course = classInput.value.trim();
 
     if (course !== '') {
         var listItem = document.createElement('li');
-        listItem.textContent = skill;
+        listItem.textContent = course;
+
+        var removeButton = document.createElement('button');
+        removeButton.innerHTML = '&times;'; // Use the "×" character for "X"
+        removeButton.addEventListener('click', function () {
+            removeSkill(listItem);
+        });
+
+        listItem.appendChild(removeButton);
 
         var classList = document.getElementById('classList');
         classList.appendChild(listItem);
 
         classInput.value = '';
     }
-});
+}
+
+function removeSkill(classItem) {
+    var classList = document.getElementById('classList');
+    classList.removeChild(classItem);
+}
+
+document.querySelector("#addClassBtn").addEventListener("click", addClass);
